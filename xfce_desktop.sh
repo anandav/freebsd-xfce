678a61e9eced62a50383564462dc152168eff163
anandav <anand.av@hotmail.com>
2022-10-29 22:06:50 +0530

    restore all code except loader.conf

--- xfce_desktop.sh
+++ xfce_desktop.sh
@@ -34,18 +34,18 @@ echo "OPTIONS_SET=OPTIMIZED_CFLAGS CPUFLAGS" >> /etc/make.conf
 ## INSTALLS BASE DESKTOP AND CORE UTILS
 echo "Installing XFCE..."
 echo ""
-pkg install -y xorg xfce xfce4-goodies slim dbus
-#pkg install -y vim sudo bash wget htop xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip 
-#pkg install -y gnome-keyring xfce4-screenshooter-plugin ristretto atril-lite gnome-font-viewer mixer mixertui qjackctl  
-#pkg install -y baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd brut clamtk vscode firefox barrier
+# pkg install -y xorg xfce xfce4-goodies slim dbus
+pkg install -y vim sudo bash wget htop xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip dbus
+pkg install -y gnome-keyring xfce4-screenshooter-plugin ristretto atril-lite gnome-font-viewer mixer mixertui qjackctl  
+pkg install -y baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd brut clamtk vscode firefox barrier
 
 # ## INSTALLS AUTOMOUNT AND FILESYSTEM SUPPORT
-# echo ""
-# echo "Enabling automount..."
-# echo ""
-# #pkg install -y automount exfat-utils fusefs-exfat fusefs-ntfs fusefs-ext2 fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs dsbmd dsbmc
-# #sysrc dsbmd_enable=YES
-# echo ""
+echo ""
+echo "Enabling automount..."
+echo ""
+pkg install -y automount exfat-utils fusefs-exfat fusefs-ntfs fusefs-ext2 fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs dsbmd dsbmc
+sysrc dsbmd_enable=YES
+echo ""
 
 ## ENABLES BASIC SYSTEM SERVICES
 echo "Enabling basic services"
@@ -73,19 +73,19 @@ then
 else fi
 
 ## CONFIGURES AUTOMOUNT FOR THE REGULAR DESKTOP USER
-#touch /usr/local/etc/automount.conf
-#echo "USERUMOUNT=YES" >> /usr/local/etc/automount.conf
-#echo "USER=$user" >> /usr/local/etc/automount.conf
-#echo "FM='thunar'" >> /usr/local/etc/automount.conf
-#echo "NICENAMES=YES" >> /usr/local/etc/automount.conf
+touch /usr/local/etc/automount.conf
+echo "USERUMOUNT=YES" >> /usr/local/etc/automount.conf
+echo "USER=$user" >> /usr/local/etc/automount.conf
+echo "FM='thunar'" >> /usr/local/etc/automount.conf
+echo "NICENAMES=YES" >> /usr/local/etc/automount.conf
 
 ## SPECIAL PERMISSIONS FOR USB DRIVES AND WEBCAM
-#echo "perm    /dev/da0        0666" >> /etc/devfs.conf
-#echo "perm    /dev/da1        0666" >> /etc/devfs.conf
-#echo "perm    /dev/da2        0666" >> /etc/devfs.conf
-#echo "perm    /dev/da3        0666" >> /etc/devfs.conf
-#echo "perm    /dev/video0     0666" >> /etc/devfs.conf
-#echo ""
+echo "perm    /dev/da0        0666" >> /etc/devfs.conf
+echo "perm    /dev/da1        0666" >> /etc/devfs.conf
+echo "perm    /dev/da2        0666" >> /etc/devfs.conf
+echo "perm    /dev/da3        0666" >> /etc/devfs.conf
+echo "perm    /dev/video0     0666" >> /etc/devfs.conf
+echo ""
 
 ## ADDS USER TO CORE GROUPS
 if [ ! -z "$user"]
@@ -112,21 +112,21 @@ kldload linux.ko
 sysrc linux_enable="YES"
 echo ""
 
-# ## COMPILE AND INSTALL LATEST NVIDIA DRIVERS FOR MODERN CARDS (GTX 1*** / RTX Series)
-# ## Please, change /usr/ports/x11/nvidia-driver to /usr/ports/x11/nvidia-driver-470 or nvidia-driver-390 if you use GeForce GT or Titan series card.
-# ## If you're using a legacy Nvidia GeForce card please use nvidia-driver-304 instead
-# echo "Compiling Nvidia drivers..."
-# echo ""
-# cd /usr/ports/x11/nvidia-driver
-# make install clean BATCH=yes
-# cd /usr/ports/x11/nvidia-settings
-# make install clean BATCH=yes
-# cd /usr/ports/x11/nvidia-xconfig
-# make install clean BATCH=yes
-# nvidia-xconfig
-# echo ""
-# echo "Nvidia drivers compiled"
-# echo ""
+## COMPILE AND INSTALL LATEST NVIDIA DRIVERS FOR MODERN CARDS (GTX 1*** / RTX Series)
+## Please, change /usr/ports/x11/nvidia-driver to /usr/ports/x11/nvidia-driver-470 or nvidia-driver-390 if you use GeForce GT or Titan series card.
+## If you're using a legacy Nvidia GeForce card please use nvidia-driver-304 instead
+echo "Compiling Nvidia drivers..."
+echo ""
+cd /usr/ports/x11/nvidia-driver
+make install clean BATCH=yes
+cd /usr/ports/x11/nvidia-settings
+make install clean BATCH=yes
+cd /usr/ports/x11/nvidia-xconfig
+make install clean BATCH=yes
+nvidia-xconfig
+echo ""
+echo "Nvidia drivers compiled"
+echo ""
 
 ## FreeBSD SYSTEM TUNING FOR BEST DESKTOP EXPERIENCE
 echo "Optimizing system parameters and firewall..."
